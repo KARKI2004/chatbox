ff874b12d316a42c7ee95eb32b804eefc4761b37
Suyog Name <karkisuyognp@gmail.com>
2025-10-14 14:58:08 -0500

    Updated chatcontainer to fix the `avatarimg keeps getting attached to sent messages` issue

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT
 
 
 app.use(cookieParser());
-app.use(express.json());
+app.use(express.json({limit: "15mb"}));
 app.use(
     cors({
     origin: "http://localhost:5173",
